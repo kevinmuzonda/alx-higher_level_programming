aa9385b09ce1dd1ed8a6e4e4837b0518685e6736
Samswagz <ilesamuel53@gmail.com>
2023-12-19 00:25:51 +0200

    err_exceptions

---
+++ 0x05-python_exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        x = fct(*args)
+        return x
+    except Exception as a:
+        print("Exception: {}".format(a), file=sys.stderr)
